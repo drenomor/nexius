3c15bc3a6a3355b1579f03805ff3a9b39538aaed
Légrádi János <johnnyfromhun@gmail.com>
2020-09-14 10:21:41 +0200

    (amountNumber) Minimum Maximum termék vásárlásának beállítása.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ function calcAmount() {
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
     let amount = parseInt(amountInput.value) * price;
-    
-    showAmount.innerHTML = amount;
+    let amountNumber = parseInt(amountInput.value);
+
+    if( amountNumber > 10) {
+        alert("Maximum 10 teméket vásárolhat!");
+        return;
+    } else if ( amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnia!")
+    } else {
+        let amountNumber = parseInt( amountInput.value) * price;
+        showAmount.innerHTML = amount;
+    }
 }
