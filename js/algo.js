9dcdc70be0f21ef8dbdf00f03066b549ade0b3aa
drenomor <johnnyfromhun@gmail.com>
2020-09-18 16:21:25 +0200

    algo.js hozzáadva

---
+++ js/algo.js
@@ -0,0 +1,14 @@
+/*
+Kávéfőző bekapcsolása
+IF nincs elég kávé THEN
+    Kávé Hozzáadása
+ENDIF
+IF nincs elég víz THEN
+    Víz hozzáadása
+ENDIF
+WHILE nem melegedett fel
+    10 mp VÁRAKOZÁS
+ENDWHILE
+Csésze odahelyezése
+Gomb megnoymása
+*/
